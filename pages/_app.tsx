56a6d59e3ef8bb030eef41d2c32c4a6b2674c260
tnantoka <tnantoka@bornneet.com>
2022-07-01 22:23:22 +0900

    feat: project base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>tfjs-sandbox</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <footer>
+        <Link href="/">tfjs-sandbox</Link> by{' '}
+        <a
+          href="https://twitter.com/tnantoka"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @tnantoka
+        </a>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/hello.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next';
+
+export default function Hello() {
+  return <h1>Hello</h1>;
+}
